b62923462f33645ad729f8f371f1f14dee83ca9b
micafinn <micafinn@gmail.com>
2017-04-04 12:50:09 +0200

    Add ability to submit a new workout

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,15 +1,24 @@
 class WorkoutsController < ApplicationController
+  before_action :find_workout, only: [:show, :edit, :update, :destroy]
 
   def index
   end
 
   def show
+
   end
 
   def new
+    @workout = Workout.new
   end
 
   def create
+    @workout = Workout.new(workout_params)
+    if @workout.save
+      redirect_to @workout
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -24,8 +33,10 @@ class WorkoutsController < ApplicationController
   private
 
   def workout_params
+    params.require(:workout).permit(:date, :workout, :mood, :length)
   end
 
   def find_workout
+    @workout = Workout.find(params[:id])
   end
 end
